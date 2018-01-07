1cf807739e298604cf24077320a7807775ab338b
Tim Cuthbertson <tim@gfxmonk.net>
2018-01-07 21:46:58 +1100

    add SurfaceQuartz

---
+++ Sources/Cairo/SurfaceQuartz.swift
@@ -0,0 +1,22 @@
+//
+//  SurfaceQuartz.swift
+//  Cairo
+//
+//  Copyright © 2017 PureSwift. All rights reserved.
+//
+
+import CCairo
+
+#if os(macOS)
+
+    import Cocoa
+    public extension Surface {
+        public final class Quartz : Surface {
+            public init(graphicsContext: CGContext, width: Int, height: Int) throws {
+                let internalPointer = cairo_quartz_surface_create_for_cg_context( graphicsContext, UInt32(width), UInt32(height))!
+                try super.init(internalPointer)
+            }
+        }
+    }
+
+#endif
